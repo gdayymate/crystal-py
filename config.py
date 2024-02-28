dfff52c4594084117ba68b62170fb4fafd9c63f8
Christian Mate <161172225+gdayymate@users.noreply.github.com>
2024-02-28 09:50:08 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration file containing constants used throughout the blockchain network
+
+SEED_LENGTH = 32
+DIFFICULTY = 4
+EPOCHS = {
+   'start': 0,
+   'duration': 10 * 60  # Epoch length set to 10 minutes
+}
+NUM_PRODUCERS = 10
